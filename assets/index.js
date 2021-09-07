cc16d34f9187b69fc446195cc19307a4e993308f
Oscar Iván Ortiz <oior986@hotmail.com>
2021-09-07 08:56:22 -0500

    Primer plugin creado

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins: [/* new AutoPlay() */] });
+
+const playButton = document.getElementById("buttonPlay")
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.getElementById("buttonMute");
+muteButton.onclick = () => player.toggleMute();
